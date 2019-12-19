e774959b11efe644bd3f1987f0960dbf156e06cc
sloterr <50946629+sloterr@users.noreply.github.com>
2019-12-19 23:37:29 +0300

    token controller

--- frontend/angularui/src/app/services/auth.service.ts
+++ frontend/angularui/src/app/services/auth.service.ts
@@ -11,18 +11,32 @@ import { LoginUser } from '../component/login/login';
 export class AuthService {
   constructor(private http: HttpClient) { }
   path = environment.path
-
+  TOKEN_KEY = "token"
   register(registerUser: RegisterUser) {
     let headers = new HttpHeaders()
     headers.append("Content-Type", "application/json")
     this.http.post(this.path + '/user/register', registerUser, { headers: headers }).subscribe()
   }
-  
+
   login(loginUser: LoginUser) {
     let headers = new HttpHeaders()
     headers.append("Content-Type", "application/json")
-    this.http.post(this.path + '/user/login', loginUser, { headers: headers }).subscribe(data=>{
+    this.http.post(this.path + '/user/login', loginUser, { headers: headers }).subscribe(data => {
       alert(data)
     })
+  }
+
+  saveToken(token) {
+    localStorage.setItem(this.TOKEN_KEY, token)
+  }
+  logOut() {
+    localStorage.removeItem(this.TOKEN_KEY)
+  }
+
+  get isAuthenticated() {
+    return !!localStorage.getItem(this.TOKEN_KEY)
+  }
+  get token() {
+    return localStorage.getItem(this.TOKEN_KEY)
   }
 }
